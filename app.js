c1bd0e13d26980dd3c1eefc126a08847046ba351
Jhonatan Montiel <jhonatanmontiel47@gmail.com>
2025-08-20 14:46:42 -0500

    Crea una función para ingresar los datos y añadirlos a un array

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+//Desarrolla una función, que permita al usuario ingresar un nombre en el campo de texto y añadirlo a la lista de amigos creada anteriormente.
+
 let amigos = [];
+
+function añadirNombre(){
+    let nombreIngresado = document.getElementById('amigo').value;
+
+   amigos.push(nombreIngresado);
+
+   console.log(amigos);
+
+}
