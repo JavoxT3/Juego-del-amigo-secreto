c25bf0bd4eda212a2f817eecec4bda645dffde8d
Jhonatan Montiel <jhonatanmontiel47@gmail.com>
2025-08-22 12:45:15 -0500

    Añade validacion de dos nombres, Y Función de reiniciar todo

--- app.js
+++ app.js
@@ -8,43 +8,53 @@ function LimpiaCampoDeTexto() {
     document.querySelector('#amigo').value = '';
 }
 
-function añadirNombre() { 
-    let nombreIngresado = document.getElementById('amigo').value;
-    
-    if (nombreIngresado.trim() === "") {
-        alert('Por favor inserte un nombre')
-        return;
-    }   
-    
+    function añadirNombre() { 
+        let nombreIngresado = (document.getElementById('amigo').value).trim();
+        
+        if (nombreIngresado === '') {
+            alert('Por favor inserte un nombre')
+            return;
+        }   
+        
 
-    amigos.push(nombreIngresado);
-    LimpiaCampoDeTexto();
-    
-    mostrarLista();
-    console.log(amigos);
+        amigos.push(nombreIngresado);
+        LimpiaCampoDeTexto();
+        
+        mostrarLista();
+        console.log(amigos);
 
-}
-
-function mostrarLista() {
-    let lista = document.querySelector('#listaAmigos');
-    lista.innerHTML = "";
-    
-    amigos.forEach(function(amigo) {
-        lista.innerHTML += `<li>${amigo}</li>`;
-    });
-}
-
-function sortearAmigo() {
-    if (amigos.length === 0) {
-        alert('La lista esta vacia, Ingresa un nombre')
-        return;
     }
 
-    let indiceSorteado = Math.floor(Math.random()*amigos.length);
-    let amigoSorteado = amigos[indiceSorteado];
+    function mostrarLista() {
+        let lista = document.querySelector('#listaAmigos');
+        lista.innerHTML = "";
+        
+        amigos.forEach(function(amigo) {
+            lista.innerHTML += `<li>${amigo}</li>`;
+        });
+    }
 
-    console.log (`El amigo elegido es: ${amigoSorteado}`);
+    function sortearAmigo() {
+        if (amigos.length === 0) {
+            alert('La lista esta vacia, Ingresa un nombre')
+            return;
+        } if (amigos.length < 2) {
+            alert('Ingrese al menos 2 nombres para hacer el sorteo');
+            return; 
+        }
+
+        let indiceSorteado = Math.floor(Math.random()*amigos.length);
+        let amigoSorteado = amigos[indiceSorteado];
+
+        console.log (`El amigo elegido es: ${amigoSorteado}`);
 
         let amigoescogido = document.getElementById('resultado')
         amigoescogido.innerHTML = `<li> El amigo elegido es:<br><strong>${amigoSorteado}</strong></li>`;
     }
+
+function reiniciar(){
+    amigos = [];
+    document.querySelector('#listaAmigos').innerHTML = "";
+    document.querySelector('#resultado').innerHTML = "";
+    LimpiaCampoDeTexto();
+}
